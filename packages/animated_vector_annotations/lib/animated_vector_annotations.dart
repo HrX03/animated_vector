7516444104d2af6ad436d922815305b33d8be8cf
Davide Bianco <dn.bianco03@gmail.com>
2023-12-29 18:12:40 +0100

    Add docs to animated_vector_annotations

--- packages/animated_vector_annotations/lib/animated_vector_annotations.dart
+++ packages/animated_vector_annotations/lib/animated_vector_annotations.dart
@@ -1,3 +1,6 @@
-library;
+/// A library to use in tandem with `package:animated_vector` and `package:animated_vector_gen`.
+///
+/// Contains annotations for code generation, such as [ShapeshifterAsset].
+library animated_vector_annotations;
 
 export 'src/animated_vector_annotations.dart';
