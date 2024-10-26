ccc32a11947922a3aad0d657eb32116733f66c2b
Davide Bianco <dn.bianco03@gmail.com>
2024-10-26 12:17:30 +0200

    Export libraries for generated files

--- packages/animated_vector_annotations/lib/animated_vector_annotations.dart
+++ packages/animated_vector_annotations/lib/animated_vector_annotations.dart
@@ -3,4 +3,24 @@
 /// Contains annotations for code generation, such as [ShapeshifterAsset].
 library animated_vector_annotations;
 
+export 'dart:ui' show Color, Size, StrokeCap, StrokeJoin;
+export 'package:animated_vector/animated_vector.dart'
+    show
+        AnimatedVectorData,
+        AnimationInterval,
+        AnimationStep,
+        ClipPathAnimationProperties,
+        ClipPathElement,
+        ConstColorTween,
+        ConstPathDataTween,
+        ConstTween,
+        GroupAnimationProperties,
+        GroupElement,
+        PathAnimationProperties,
+        PathData,
+        PathElement,
+        RootVectorAnimationProperties,
+        RootVectorElement,
+        ShapeShifterCurves;
+
 export 'src/animated_vector_annotations.dart';
